b7a5c1032dd1c03dc82016243dae3585a9557e07
Scut-ChenJL <38501974+Scut-ChenJL@users.noreply.github.com>
2018-07-10 10:09:19 +0800

    7.10
    创建打卡基本界面做好了，发送到后台那段还要调试一下

--- frontend/pages/mainPage/mainPage.js
+++ frontend/pages/mainPage/mainPage.js
@@ -2,7 +2,66 @@
 var app = getApp()
 
 Page({
+  data: {
+    // text:"这是一个页面"
+    picker1Value: 0,
+    picker1Range: ['1天', '2天', '3天', '5天', '一周', '两周', '一个月'],
+    timeValue: '08:08',
+    dateValue: '2018-7-11'
+  },
+  normalPickerBindchange: function (e) {
+    this.setData({
+      picker1Value: e.detail.value
+    })
+  },
+  timePickerBindchange: function (e) {
+    this.setData({
+      timeValue: e.detail.value
+    })
+  },
+  datePickerBindchange: function (e) {
+    this.setData({
+      dateValue: e.detail.value
+    })
+  },
 
+  data: {
+    logs: []
+  },
+  onLoad: function () {
+    this.getdata();
+
+  },
+
+  getdata: function () {//定义函数名称
+    var that = this;   // 这个地方非常重要，重置data{}里数据时候setData方法的this应为以及函数的this, 如果在下方的sucess直接写this就变成了wx.request()的this了
+    wx.request({
+      url: 'http://www.w.com',//请求地址
+      data: {//发送给后台的数据
+        days: picker1Value,
+        time: timeValue,
+        date: dateValue
+      },
+      header: {//请求头
+        "Content-Type": "applciation/json"
+      },
+      method: "GET",//get为默认方法/POST
+      success: function (res) {
+        console.log(res.data);//res.data相当于ajax里面的data,为后台返回的数据
+        that.setData({//如果在sucess直接写this就变成了wx.request()的this了.必须为getdata函数的this,不然无法重置调用函数
+
+          logs: res.data.result
+
+        })
+
+      },
+      fail: function (err) { },//请求失败
+      complete: function () { }//请求完成后执行的函数
+    })
+  },
+
+
+  
   /**
    * 页面的初始数据
    */
