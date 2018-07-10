c78bcb6e02aac3222fa8ebdce3d4287a96c25897
Scut-ChenJL <38501974+Scut-ChenJL@users.noreply.github.com>
2018-07-10 11:53:27 +0800

    discovry
    发现页搞了个搜索栏和图片轮播

--- frontend/pages/discovery/discovery.js
+++ frontend/pages/discovery/discovery.js
@@ -1,140 +1,208 @@
-// pages/discovery/dicovery.js
-var app = getApp()
-var sectionData = null
-var currentSectionIndex = 0
-Page({
+/***
+ * // 定义数据格式
+ * "wxSearchData":{
+ *  configconfig:{
+ *    style: "wxSearchNormal"
+ *  },
+ *  view:{
+ *    hidden: true,
+ *    searchbarHeght: 20
+ *  }
+ *  hotKeys:[],//自定义热门搜索
+ *  his:[]//历史搜索关键字
+ *  value
+ * }
+ */
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    sections : {},
-    groups : {}
-  },
+// 提示集合
+var __tipKeys = [];
+// 搜索回调函数 
+var __searchFunction = null;
+// 返回函数 
+var __goBackFunction = null;
+// 应用变量
+var __that = null;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this
-    //获取分类信息，需要一个需要请求后端数据库
-    wx.request({
-      url: 'http://127.0.0.1/groups/hot',
-      data: {},
-      success: function (res) {
-        sectionData = res.data.sections;
-        sectionData[0]['active'] = true //默认选中第一个分类
-        that.loadGroups(sectionData[0]['section_id'])
-        that.setData({
-          sections: sectionData
-        });
-      }
-    })
-  },
+// 初始化函数
+function init(that, hotKeys, tipKeys, searchFunction, goBackFunction) {
 
-  onSectionClicked: function (e) {
-    // 用户选中一个分类，会向服务器请求一次数据
-    var sid = e.currentTarget.dataset.sid;
-    //刷新选中状态
-    for (var i in sectionData) {
-      if (sectionData[i]['section_id'] == sid) {
-        sectionData[i]['active'] = true
-        currentSectionIndex = i
-      }
-      else
-        sectionData[i]['active'] = false
-    }
-    this.setData({
-      sections: sectionData
-    });
-    //加载小组信息
-    if (sectionData[i]['groups']) {
-      this.setData({
-        groups: sectionData[i]['groups']
-      });
-    } else {
-      this.loadGroups(sid)
-    }
-  },
+  __that = that;
+  __tipKeys = tipKeys;
+  __searchFunction = searchFunction;
+  __goBackFunction = goBackFunction;
 
-  loadGroups: function (section_id) {
-    var that = this
-    //获取小组列表
-    wx.request({
-      // 开发时使用本地的数据库
-      url: 'http://127.0.0.1/groups/group',
-      method: 'POST',
-      data: {
-        section_id: section_id,
-        start_id: 0,
-        limit: 10 // 限制了一次加载十个小组的信息进行显示
-      },
-      header: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      success: function (res) {
-        var groupsData = res.data.groups;
-        sectionData[currentSectionIndex]['groups'] = groupsData
-        that.setData({
-          groups: groupsData
-        });
-      }
-    })
-  },
-  // 点击一个小组的话查看具体信息
-  onGroupClicked: function (e) {
-    var gid = e.currentTarget.dataset.gid
-    wx.navigateTo({
-      url: '/pages/detail/detail?group_id=' + gid
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  var temData = {};
+  var barHeight = 43;
+  var view = {
+    barHeight: barHeight
   }
-})
+  temData.hotKeys = hotKeys;
+
+  wx.getSystemInfo({
+    success: function (res) {
+      var wHeight = res.windowHeight;
+      view.seachHeight = wHeight - barHeight;
+      temData.view = view;
+      __that.setData({
+        wxSearchData: temData
+      });
+    }
+  });
+
+  getHisKeys(__that);
+}
+
+// 搜索框输入时候操作
+function wxSearchInput(e) {
+  var inputValue = e.detail.value;
+  // 页面数据
+  var temData = __that.data.wxSearchData;
+  // 寻找提示值 
+  var tipKeys = [];
+  if (inputValue && inputValue.length > 0) {
+    for (var i = 0; i < __tipKeys.length; i++) {
+      var mindKey = __tipKeys[i];
+      // 包含字符串
+      if (mindKey.indexOf(inputValue) != -1) {
+        tipKeys.push(mindKey);
+      }
+    }
+  }
+  // 更新数据
+  temData.value = inputValue;
+  temData.tipKeys = tipKeys;
+  // 更新视图
+  __that.setData({
+    wxSearchData: temData
+  });
+}
+
+// 清空输入
+function wxSearchClear() {
+  // 页面数据
+  var temData = __that.data.wxSearchData;
+  // 更新数据
+  temData.value = "";
+  temData.tipKeys = [];
+  // 更新视图
+  __that.setData({
+    wxSearchData: temData
+  });
+}
+
+// 点击提示或者关键字、历史记录时的操作
+function wxSearchKeyTap(e) {
+  search(e.target.dataset.key);
+}
+
+// 确任或者回车
+function wxSearchConfirm(e) {
+  var key = e.target.dataset.key;
+  if (key == 'back') {
+    __goBackFunction();
+  } else {
+    search(__that.data.wxSearchData.value);
+  }
+}
+
+function search(inputValue) {
+  if (inputValue && inputValue.length > 0) {
+    // 添加历史记录
+    wxSearchAddHisKey(inputValue);
+    // 更新
+    var temData = __that.data.wxSearchData;
+    temData.value = inputValue;
+    __that.setData({
+      wxSearchData: temData
+    });
+    // 回调搜索
+    __searchFunction(inputValue);
+  }
+}
+
+// 读取缓存
+function getHisKeys() {
+  var value = [];
+  try {
+    value = wx.getStorageSync('wxSearchHisKeys')
+    if (value) {
+      // Do something with return value
+      var temData = __that.data.wxSearchData;
+      temData.his = value;
+      __that.setData({
+        wxSearchData: temData
+      });
+    }
+  } catch (e) {
+    // Do something when catch error
+  }
+}
+
+// 添加缓存
+function wxSearchAddHisKey(inputValue) {
+  if (!inputValue || inputValue.length == 0) {
+    return;
+  }
+  var value = wx.getStorageSync('wxSearchHisKeys');
+  if (value) {
+    if (value.indexOf(inputValue) < 0) {
+      value.unshift(inputValue);
+    }
+    wx.setStorage({
+      key: "wxSearchHisKeys",
+      data: value,
+      success: function () {
+        getHisKeys(__that);
+      }
+    })
+  } else {
+    value = [];
+    value.push(inputValue);
+    wx.setStorage({
+      key: "wxSearchHisKeys",
+      data: value,
+      success: function () {
+        getHisKeys(__that);
+      }
+    })
+  }
+}
+
+// 删除缓存
+function wxSearchDeleteAll() {
+  wx.removeStorage({
+    key: 'wxSearchHisKeys',
+    success: function (res) {
+      var value = [];
+      var temData = __that.data.wxSearchData;
+      temData.his = value;
+      __that.setData({
+        wxSearchData: temData
+      });
+    }
+  })
+}
+
+// 导出接口
+module.exports = {
+  init: init, //初始化函数
+  wxSearchInput: wxSearchInput,// 输入变化时的操作
+  wxSearchKeyTap: wxSearchKeyTap, // 点击提示或者关键字、历史记录时的操作
+  wxSearchDeleteAll: wxSearchDeleteAll, // 删除所有的历史记录
+  wxSearchConfirm: wxSearchConfirm, // 搜索函数
+  wxSearchClear: wxSearchClear,  // 清空函数
+}
+
+var app = getApp()
+Page({
+  data: {
+    movies: [
+      { url: 'http://www.23book.com/upload/2017/07/27/18b2ab0cc253ee6f.jpg' },
+      { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1531203818898&di=ec459cca510d610500dfd024fe6016a6&imgtype=0&src=http%3A%2F%2Fcdn.k618img.cn%2Fnews.k618.cn%2Froll%2F201709%2FW020170901388076680765.jpg' },
+      { url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1531203848824&di=83c5b0f332e4f9843fb10709f4985376&imgtype=0&src=http%3A%2F%2Fwww.kentrexs.cn%2Fuploads%2Fallimg%2F161230%2F787-16123010005c10.jpg' },
+      { url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1405626712,1194876936&fm=27&gp=0.jpg' }
+    ]
+  },
+  onLoad: function () {
+  }
+}) 
